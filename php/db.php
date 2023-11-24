d2aee5f907d1754ba6a5617cb28ed9ded918998c
JoelOtero <119890772+JoelOteroMas@users.noreply.github.com>
2023-11-24 19:21:03 +0100

    Delete php/db.php

--- php/db.php
+++
@@ -1,8 +0,0 @@
-<?php  //conecta la base de dades
-$conn = mysqli_connect(
-    'localhost',  //nom server
-    'root',  //nom user de la db
-    '',  //contrasenya de la db
-    'PBE_BD'  //nom de la db
-);
-?>
