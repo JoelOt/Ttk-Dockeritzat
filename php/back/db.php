0e668769047f742609656b3362de163d2eae2385
JoelOtero <119890772+JoelOteroMas@users.noreply.github.com>
2023-11-24 19:22:10 +0100

    Create db.php

---
+++ php/back/db.php
@@ -0,0 +1,8 @@
+<?php  //conecta la base de dades
+$conn = mysqli_connect(
+    'localhost',  //nom server
+    'root',  //nom user de la db
+    '',  //contrasenya de la db
+    'PBE_BD'  //nom de la db
+);
+?>
